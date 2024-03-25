dd7d9374544eaa7c0d91e6e1c2bfaa0472de19a6
sushant sunar <sushantsunar20@gmail.com>
2024-03-25 21:49:02 +0545

    Loops

---
+++ Loops/sum of ten number.cpp
@@ -0,0 +1,15 @@
+#include<stdio.h>
+#include<conio.h>
+int main()
+{
+	int i,n,sum=0;
+	for(i=0;i<10;i++)
+	{
+		printf("enter a number");
+		scanf("%d",&n);
+		sum=sum+n;
+	}
+printf("%d",sum);
+ 
+getch();
+}
---
+++ Loops/while loop.cpp
@@ -0,0 +1,12 @@
+#include<stdio.h>
+#include<conio.h>
+int main()
+{
+int count=1;
+	while(count<=10)
+{
+	printf("%d",count);
+	count++;
+}
+getch();
+}
